b691d18358d9042435f5eb3c0b26041fa227417f
Wyatt_SG <schultewya@gmail.com>
2022-05-03 23:34:58 -0500

    added proxy and refactored to api limits

--- src/data-fetcher.js
+++ src/data-fetcher.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import https from 'https';
+import http from 'http';
 
 /**
  * Gets items data from the Target API
@@ -6,8 +8,22 @@ import axios from "axios";
  * @returns raw items data
  */
 export const getItems = async (offset) => {
+    const targetProductsUrl = `https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&category=5q0ga&channel=WEB&count=28&default_purchasability_filter=true&include_sponsored=true&offset=${offset}&page=%2Fc%2F5q0ga&platform=desktop&pricing_store_id=1944&useragent=Mozilla%2F5.0+%28Macintosh%3B+Intel+Mac+OS+X+10_15_7%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F100.0.4896.60+Safari%2F537.36&visitor_id=0180869970B00201973A7206284F54AF`;
     let items = [];
-    await axios.get(`https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&category=5q0ga&channel=WEB&count=28&default_purchasability_filter=true&include_sponsored=true&offset=${offset}&page=%2Fc%2F5q0ga&platform=desktop&pricing_store_id=1944&useragent=Mozilla%2F5.0+%28Macintosh%3B+Intel+Mac+OS+X+10_15_7%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F100.0.4896.60+Safari%2F537.36&visitor_id=0180869970B00201973A7206284F54AF`)
+    await axios.post('https://de.hideproxy.me/includes/process.php?action=update', new URLSearchParams({ u: targetProductsUrl }), {
+        headers: {
+            'authority': `de.hideproxy.me`,
+            'origin': 'https://hide.me',
+            'content-type': 'application/x-www-form-urlencoded',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'referer': 'https://hide.me/',
+            'accept-language': 'en-US,en;q=0.9,sv;q=0.8,hy;q=0.7',
+            'cookie': `s=qq5qmklp838e3kuufkjvtms5pb`
+        },
+        httpAgent: new http.Agent({ keepAlive: true }),
+        httpsAgent: new https.Agent({ keepAlive: true })
+    })
         .then((response) => {
             if (response.status == 200) {
                 items = response.data.data.search.products;
--- src/script-logic.js
+++ src/script-logic.js
@@ -1,10 +1,10 @@
-import { getItems, getTotalItemCount } from "./data-fetcher.js";
+import { getItems } from "./data-fetcher.js";
 import { saveItems } from "./db-connector.js";
 
 const startScraping = async () => {
-    const totalItemCount = await getTotalItemCount();
     // Max product set for the Target API is 28
-    for (let itemOffset = 0; itemOffset < totalItemCount; itemOffset += 28) {
+    // Max product offset for the Target API is 1199
+    for (let itemOffset = 0; itemOffset < 1199; itemOffset += 28) {
         const items = await getItems(itemOffset)
         await saveItems(items);
     }
